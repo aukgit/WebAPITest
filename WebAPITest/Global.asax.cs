fa8ab070b70c5c5db7d576999ce19a58f2532ad9
Alim Ul Karim <devorg.bd@gmail.com>
2015-03-12 14:21:11 +0600

    add this line in global
    GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling
    = Newtonsoft.Json.ReferenceLoopHandling.Ignore;

--- WebAPITest/Global.asax.cs
+++ WebAPITest/Global.asax.cs
@@ -18,6 +18,7 @@ namespace WebAPITest
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            // add this line
             GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
         }
     }
