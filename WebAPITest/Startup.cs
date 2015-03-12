656f9dbc92b88452a9782626c0bac6f3cb9dd0d3
Alim Ul Karim <devorg.bd@gmail.com>
2015-03-12 14:20:16 +0600

    web api tes

---
+++ WebAPITest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAPITest.Startup))]
+namespace WebAPITest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
